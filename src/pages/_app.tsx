4c4b613bd9824b31487be8774315ad50136ca9e1
jroiceee <jroicealdeza@gmail.com>
2023-02-25 22:15:11 +0800

    add react-bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import "../styles/scss/app.scss"
+import "../styles/globals.css"
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
