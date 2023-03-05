1d21b2db1c1968d9078b519dad0428cdb30a9386
jroiceee <jroicealdeza@gmail.com>
2023-03-05 15:53:31 +0800

    add api-key-component

---
+++ src/components/api-keys-page-components/api-key-component.tsx
@@ -0,0 +1,49 @@
+import { FormEvent, useCallback, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+export default function ApiKeyComponent() {
+  const [apiKeyState, setApiKeyState] = useState<string>("");
+  const [showKeyState, setShowKeyState] = useState<string>("password");
+
+  const showKeyStateToggler = useCallback(() => {
+    if (showKeyState === "password") {
+      setShowKeyState("text");
+      return;
+    }
+    setShowKeyState("password");
+  }, [showKeyState]);
+
+  const generateAPIKey = useCallback(() => {}, []);
+
+  return (
+    <div>
+      <Form
+        onSubmit={(e: FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+        }}
+      >
+        <Form.Control
+          style={{ maxWidth: "300px" }}
+          type={showKeyState}
+          placeholder="Generate your API Key"
+          value={apiKeyState}
+          readOnly
+        />
+
+        <div className="d-flex align-items-center gap-1 mt-1">
+          <Form.Check
+            type="checkbox"
+            id="show-key"
+            onChange={showKeyStateToggler}
+          />
+          <span style={{ fontSize: "0.8rem" }}>Show key</span>
+        </div>
+      </Form>
+
+      <div className="mt-2 d-flex gap-2">
+        <Button variant="primary">Generate</Button>
+        <Button variant="outline-danger">Delete</Button>
+      </div>
+    </div>
+  );
+}
--- src/pages/_api-keys/index.tsx
+++ src/pages/_api-keys/index.tsx
@@ -1,11 +1,55 @@
 import MainLayout from "@/components/layouts/main-layout";
 import HeadWrapper from "@/components/head-wrapper";
+import { Button, Container } from "react-bootstrap";
+import Link from "next/link";
+import ApiKeyComponent from "@/components/api-keys-page-components/api-key-component";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function ApiKeysIndex() {
-    return(
-        <MainLayout>
-            <HeadWrapper title={"API Keys"}/>
-            <p>This page is under construction</p>
-        </MainLayout>
-    )
-};
+  const auth = useAuth0();
+
+  return (
+    <MainLayout>
+      <HeadWrapper title={"API Keys"} />
+      <Container className="mt-3">
+        <main>
+          <article>
+            <h2>API Keys</h2>
+            <p>
+              An API key is needed to access resources from our API. This key is
+              private and is not meant to be shared with other people.
+            </p>
+            <Link
+              href={
+                "https://help.openai.com/en/articles/5112595-best-practices-for-api-key-safety#:~:text=Use%20Environment%20Variables%20in%20place,of%20the%20variable%20to%20OPENAI_API_KEY."
+              }
+              target="_blank"
+            >
+              Best Practices for API Key Safety.
+            </Link>
+          </article>
+          <hr />
+          <article>
+            <h5>Your API Keys</h5>
+            <p>
+              As of now, Phrase API only allows <b>one</b> API key for each
+              user. You can use the same API Key for all of your applications.
+            </p>
+          </article>
+          {auth.isAuthenticated ? (
+            <ApiKeyComponent />
+          ) : (
+            <Button
+              variant="dark"
+              onClick={() => {
+                auth.loginWithRedirect();
+              }}
+            >
+              Sign in to access your API keys.
+            </Button>
+          )}
+        </main>
+      </Container>
+    </MainLayout>
+  );
+}
