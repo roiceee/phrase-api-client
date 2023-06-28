25da82015ebf2d5cdac1955a9be3e55b250004b8
jroiceee <jroicealdeza@gmail.com>
2023-06-28 15:26:04 +0800

    add margin y

--- src/components/loading-div.tsx
+++ src/components/loading-div.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "react-bootstrap";
 
 function LoadingDiv() {
   return (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center my-2">
       <Spinner animation="border" variant="dark" />
     </div>
   );
