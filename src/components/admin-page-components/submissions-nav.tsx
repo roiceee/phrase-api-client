c5a06becfa8ffcf33c1414560fbe49f8120075cf
jroiceee <jroicealdeza@gmail.com>
2023-07-07 13:40:04 +0800

    add accepted page

--- src/components/admin-page-components/submissions-nav.tsx
+++ src/components/admin-page-components/submissions-nav.tsx
@@ -12,38 +12,48 @@ function SubmissionsNav() {
 
   return (
     <>
-    <Nav variant="pills" as="ul" className="gap-2">
-      <Nav.Item as="li">
-        <Nav.Link
-          as={Link}
-          href="/admin/submissions/all"
-          eventKey="all"
-          active={checkActiveExactPath("/admin/submissions/all")}
-        >
-          All
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link
-          as={Link}
-          href="/admin/submissions/pending"
-          eventKey="pending"
-          active={checkActiveExactPath("/admin/submissions/pending")}
-        >
-          Pending
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <Nav.Link
-          as={Link}
-          href="/admin/submissions/rejected"
-          eventKey="rejected"
-          active={checkActiveExactPath("/admin/submissions/rejected")}
-        >
-          Rejected
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+      <Nav variant="pills" as="ul" className="gap-2">
+        <Nav.Item as="li">
+          <Nav.Link
+            as={Link}
+            href="/admin/submissions/all"
+            eventKey="all"
+            active={checkActiveExactPath("/admin/submissions/all")}
+          >
+            All
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link
+            as={Link}
+            href="/admin/submissions/accepted"
+            eventKey="accepted"
+            active={checkActiveExactPath("/admin/submissions/accepted")}
+          >
+            Accepted
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link
+            as={Link}
+            href="/admin/submissions/pending"
+            eventKey="pending"
+            active={checkActiveExactPath("/admin/submissions/pending")}
+          >
+            Pending
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <Nav.Link
+            as={Link}
+            href="/admin/submissions/rejected"
+            eventKey="rejected"
+            active={checkActiveExactPath("/admin/submissions/rejected")}
+          >
+            Rejected
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </>
   );
 }
---
+++ src/pages/admin/submissions/accepted.tsx
@@ -0,0 +1,11 @@
+import SubmissionsLayout from "@/components/layouts/admin/submissions-layout";
+
+function AcceptedSubmissions() {
+    return ( 
+        <SubmissionsLayout>
+            <h4>Accepted Submissions</h4>
+        </SubmissionsLayout>
+     );
+}
+
+export default AcceptedSubmissions;
