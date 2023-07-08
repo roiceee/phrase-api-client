ba80c95106b0f5cc18346a6e289feeef4f83848c
jroiceee <jroicealdeza@gmail.com>
2023-07-08 14:08:47 +0800

    types for spring boot pagination interface

---
+++ src/types/phrase-admin-pagination.ts
@@ -0,0 +1,22 @@
+import Phrase from "./phrase";
+
+interface PhraseAdminPagination {
+  //make types based on spring boot pagination interface
+  content: Phrase[];
+  totalElements: number;
+  totalPages: number;
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  size: number;
+
+  number: number;
+  sort: {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  };
+  numberOfElements: number;
+}
+
+export default PhraseAdminPagination;
