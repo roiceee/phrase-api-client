63f0c4a120ca64140d537801cb71945e7a99412c
jroiceee <jroicealdeza@gmail.com>
2023-07-08 19:10:29 +0800

    implement pagination

--- src/pages/admin/submissions/all/[page].tsx
+++ src/pages/admin/submissions/all/[page].tsx
@@ -6,6 +6,7 @@ import createDefaultPhraseAdminPagination from "@/types/phrase-admin-pagination/
 import PhraseAdminPagination from "@/types/phrase-admin-pagination/phrase-admin-pagination";
 import { useAuth0 } from "@auth0/auth0-react";
 import _ from "lodash";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
@@ -20,7 +21,7 @@ function AllSubmissions() {
 
   const router = useRouter();
 
-  const fetchAllSubmissions = useCallback(async () => {
+  const fetchSubmissions = useCallback(async () => {
     setDataFetchingState("loading");
     try {
       const token = await getAccessTokenSilently();
@@ -46,12 +47,77 @@ function AllSubmissions() {
     }
   }, [getAccessTokenSilently, router]);
 
+  const renderPagination = useMemo(() => {
+    //We use the spring boot pageable interface. Only 6 pagination divs are available at the time, with references to first and last pages. Page starts with 0
+    if (submissions.empty) {
+      return;
+    }
+    const page = submissions.number;
+    const totalPages = submissions.totalPages;
+    const pageNumbers = _.range(
+      Math.max(page - 3, 0),
+      Math.min(page + 3, totalPages)
+    );
+    const renderPageNumbers = pageNumbers.map((number) => {
+      return (
+        <Link
+          className="page-link"
+          key={number}
+          href={`/admin/submissions/all/${number}`}
+        >
+          {number}
+        </Link>
+      );
+    });
+    return (
+      <nav className="d-flex flex-wrap">
+        <ul className="pagination d-flex gap-1">
+          <li className="page-item">
+            <Link className="page-link" href={`/admin/submissions/all/${0}`}>
+              &lt;&lt;
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link
+              className="page-link"
+              href={`/admin/submissions/all/${page === 0 ? page : page - 1}`}
+            >
+              &lt;
+            </Link>
+          </li>
+          {renderPageNumbers}
+          <li className="page-item">
+            <Link
+              className="page-link"
+              href={`/admin/submissions/all/${
+                page === totalPages - 1 ? page : page + 1
+              }`}
+            >
+              &gt;
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link
+              className="page-link"
+              href={`/admin/submissions/all/${totalPages - 1}`}
+            >
+              &gt;&gt;
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }, [submissions.empty, submissions.totalPages, submissions.number]);
+
   const renderSubmissions = useMemo(() => {
     if (dataFetchingState === "loading") {
       return <LoadingDiv />;
     }
     if (dataFetchingState === "error") {
       return <div>Failed to load phrases.</div>;
+    }
+    if (submissions.empty) {
+      return <div>Empty.</div>;
     }
     return submissions.content.map((submission) => {
       return (
@@ -65,19 +131,20 @@ function AllSubmissions() {
 
   useEffect(() => {
     if (router.isReady) {
-      fetchAllSubmissions();
+      fetchSubmissions();
       return;
     }
-  }, [fetchAllSubmissions, router.isReady]);
+  }, [fetchSubmissions, router.isReady]);
 
   return (
     <SubmissionsLayout>
       <div>
         <div className="mb-3">
           <h4>All Submissions</h4>
-          <RefreshButton onClick={() => fetchAllSubmissions()} />
+          <RefreshButton onClick={() => fetchSubmissions()} />
         </div>
-        {renderSubmissions}
+        <div>{renderSubmissions}</div>
+        <div className="d-flex">{renderPagination}</div>
       </div>
     </SubmissionsLayout>
   );
